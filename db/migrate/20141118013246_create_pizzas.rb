a155ba1316c97ca2d04a3146e0f78f5f1b5d6ff7
Nicole Calhoun <calhounnicole@hotmail.com>
2014-11-19 18:29:25 -0500

    initialize files

---
+++ db/migrate/20141118013246_create_pizzas.rb
@@ -0,0 +1,14 @@
+class CreatePizzas < ActiveRecord::Migration
+  def change
+    create_table :pizzas do |t|
+      t.string :name
+      t.text :toppings
+      t.integer :time_to_cook
+      t.float :cost
+      t.date :date_created
+      t.time :time_put_in
+
+      t.timestamps
+    end
+  end
+end
